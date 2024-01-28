913420dec7e2cc28b160e072e79b8de80d8b8230
AbhiRam235 <abhiramreddy212@gmail.com>
2024-01-28 11:23:16 +0530

    Addition of two numbers

---
+++ Arthematic.java
@@ -0,0 +1,13 @@
+package LocalRepo;
+import java.util.Scanner;
+public class Arthematic {
+    static void add(int a, int b){
+        System.out.println("The sum of"+ a +" and "+ b + "is ="+ (a+b));
+     }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        add(a,b);
+    }
+}
